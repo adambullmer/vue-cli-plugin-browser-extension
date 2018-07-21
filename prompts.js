d36114189879f1d12a5373553c1d1aeda7909a32
Hugo Alliaume <kocal@live.fr>
2018-07-21 23:44:51 +0200

    Fix prompt message

--- prompts.js
+++ prompts.js
@@ -3,6 +3,6 @@ module.exports = [
     name: 'optionsPage',
     type: 'confirm',
     default: false,
-    description: 'Generate an options page? (https://developer.chrome.com/extensions/options)'
+    message: 'Generate an options page? (https://developer.chrome.com/extensions/options)'
   }
 ]
