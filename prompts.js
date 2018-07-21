a941f655615f412c44dcdf55b80ec992a0f61d9a
Hugo Alliaume <kocal@live.fr>
2018-07-21 21:28:14 +0200

    Add prompt

---
+++ prompts.js
@@ -0,0 +1,8 @@
+module.exports = [
+  {
+    name: 'optionsPage',
+    type: 'confirm',
+    default: false,
+    description: 'Generate an options page? (https://developer.chrome.com/extensions/options)'
+  }
+]
